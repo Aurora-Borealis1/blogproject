e11b13729661e28aaa5a27272c5cb8e59db07c54
gongyuan <1965460697@qq.com>
2020-11-08 13:37:13 +0800

    重构项目

---
+++ api/src/main/java/com/gongyuan/model/model.java
@@ -0,0 +1,8 @@
+package com.gongyuan.model;
+
+/**
+ * @author Tww
+ * @date 2020/11/81:36 下午
+ */
+public class model {
+}
---
+++ service/src/main/java/com/gongyuan/ServiceApplication.java
@@ -0,0 +1,11 @@
+package com.gongyuan;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Tww
+ * @date 2020/11/812:59 下午
+ */
+@SpringBootApplication
+public class ServiceApplication {
+}
